8495a1269179f08693cf26e3eafcde1cd99efdba
LizzetteFlores <lizzettedeyaniraf@gmail.com>
2024-11-28 22:13:01 -0600

    Initial commit

---
+++ app/src/main/java/com/lizzetteflores/registroasistencia/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.lizzetteflores.registroasistencia
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+
+ class MainActivity: AppCompatActivity() {
+
+
+ }
